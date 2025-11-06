432eff3cf373027485b40fe8304a97d607da1a4c
Lylezgraphics <ethanglyles@gmail.com>
2025-11-06 13:55:19 -0800

    First Godot push

---
+++ final_game_2.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+@onready var marker_2d: Marker2D = $Marker2D
+
+func _ready() -> void:
+	for player in get_tree().get_nodes_in_group("Player"):
+		player.respawned.connect(_on_player_respawned.bind(player))
+		
+func _on_player_respawned(player):
+	player.global_position = marker_2d.global_position
---
+++ spikes.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().call_deferred("reload_current_scene"	)
+	
